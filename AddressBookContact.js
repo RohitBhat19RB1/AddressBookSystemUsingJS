bf78fd44dbc3d5b131c413486bb8a1bd0c7c0338
RohitBhat19RB1 <rohitbhat16860@gmail.com>
2020-11-30 20:44:46 +0530

    Added: AddressBookContact created

---
+++ AddressBookContact.js
@@ -0,0 +1,22 @@
+class AddressBookContact {
+    // property
+    firstName;
+    lastName;
+    address;
+    city;
+    state;
+    zip;
+    phoneNumber;
+    email;
+
+    //constructor
+    constructor(firstName, lastName, address, city, state, zip, phoneNumber, email) {
+        this.firstName = firstName ;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email; 
+    }
